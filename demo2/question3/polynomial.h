db1c7dc9006fabeda6caedd2cd14bbd59ff49d5b
julymiaw <julywu399@gmail.com>
2023-10-13 01:06:03 +0800

    第二次实验作业提交

---
+++ demo2/question3/polynomial.h
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+class Polynomial
+{
+public:
+    Polynomial(int t = 0);
+    Polynomial &operator=(const Polynomial &r);
+    ~Polynomial();
+    double F1a(double x);
+    double F1b(double x, int index = 0);
+    double F2a(double x);
+    double F2b(double x, int index = 0);
+    double GetCoef(int pos) { return coefArray[pos]; }
+    int GetTerms() { return terms; }
+
+private:
+    double *coefArray;
+    int capacity;
+    int terms;
+};
+
+// 默认构造函数
+Polynomial::Polynomial(int t) : terms(t)
+{
+    capacity = (terms == 0) ? 1 : t;
+    coefArray = new double[capacity];
+    default_random_engine generator(time(0));
+    uniform_real_distribution<double> distribution(0, 10);
+    for (int i = 0; i < terms; i++)
+    {
+        double randomValue = distribution(generator); // 生成随机数并填充到数组
+        coefArray[i] = stod(to_string(randomValue).substr(0, 4));
+    }
+}
+
+Polynomial &Polynomial::operator=(const Polynomial &r)
+{
+    terms = r.terms;
+    capacity = (terms == 0) ? 1 : terms;
+    delete[] coefArray;
+    coefArray = new double[capacity];
+    copy(r.coefArray, r.coefArray + terms, coefArray);
+    return *this;
+}
+
+// 析构函数
+Polynomial::~Polynomial()
+{
+    delete[] coefArray;
+}
+
+ostream &operator<<(ostream &os, Polynomial &r)
+{
+    for (int Pos = 0; Pos < r.GetTerms() - 1; Pos++)
+    {
+        os << r.GetCoef(Pos);
+        os << (r.GetCoef(Pos) ? " * x^" : "\bx^");
+        os << r.GetTerms() - Pos - 1 << " + ";
+        os << (r.GetTerms() - Pos - 1 == 1 ? "\b\b\b\b\b + " : "");
+    }
+    os << r.GetCoef(r.GetTerms() - 1);
+    return os;
+}
---
+++ demo2/question3/question3.cpp
@@ -0,0 +1,150 @@
+#include <windows.h>
+#include "polynomial.h"
+#include <fstream>
+using namespace std;
+
+double Polynomial::F1a(double x)
+{
+    double y = 0;
+    for (int Pos = 0; Pos < terms; Pos++)
+        y += coefArray[Pos] * pow(x, terms - Pos - 1);
+    return y;
+}
+
+double Polynomial::F1b(double x, int index)
+{
+    return index < terms ? coefArray[index] * pow(x, terms - index - 1) + F1b(x, index + 1) : 0;
+}
+
+double Polynomial::F2a(double x)
+{
+    double y = 0;
+    for (int Pos = 0; Pos < terms; Pos++)
+        y = y * x + coefArray[Pos];
+    return y;
+}
+
+double Polynomial::F2b(double x, int index)
+{
+    return index < terms ? F2b(x, index + 1) * x + coefArray[terms - index - 1] : 0;
+}
+
+double F3(int n, double x)
+{
+    double y = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        y += pow(x, i) / i;
+    }
+    return y;
+}
+
+double algorithms(Polynomial &f, double x, int index)
+{
+    switch (index)
+    {
+    case 0:
+        return f.F1a(x);
+    case 1:
+        return f.F1b(x);
+    case 2:
+        return f.F2a(x);
+    case 3:
+        return f.F2b(x);
+    default:
+        cout << "程序错误" << endl;
+        return -1;
+    }
+}
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    // 第一题
+    cout << "取x=1.1,计算f3(x)的值:" << F3(6, 1.1) << endl;
+
+    // 第二题
+    int n = 6;
+    Polynomial f(n);
+    cout << "已随机生成多项式f:" << f << endl;
+
+    double x = 3.2;
+    cout << "使用迭代法公式1:" << f.F1a(x) << endl;
+    cout << "使用递归法公式1:" << f.F1b(x) << endl;
+    cout << "使用迭代法公式2:" << f.F2a(x) << endl;
+    cout << "使用递归法公式2:" << f.F2b(x) << endl;
+
+    // n的取值数组
+    int n_values[] = {1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100};
+
+    // 一组x的取值
+    double x_values[] = {8.27336, 9.60413, 3.79897, 6.29539, 2.91809}; // 你可以添加更多x值
+
+    const char *algorithm_names[] = {"F1a", "F1b", "F2a", "F2b"};
+
+    Polynomial Polynomials[sizeof(n_values) / sizeof(int)];
+
+    for (int j = 0; j < sizeof(n_values) / sizeof(int); j++)
+    {
+        int n = n_values[j];
+        Polynomial f(n); // 创建一个Polynomial对象
+        Polynomials[j] = f;
+    }
+
+    // 打开一个文件以写入数据
+    ofstream outputFile("output.csv");
+
+    // 检查文件是否成功打开
+    if (!outputFile.is_open())
+    {
+        cerr << "无法打开文件" << std::endl;
+        return 1;
+    }
+
+    // 创建表格的表头
+    outputFile << "n\t";
+    for (int n : n_values)
+    {
+        outputFile << n << "\t";
+    }
+    outputFile << "\n";
+
+    // 遍历四种算法
+    for (int i = 0; i < 4; i++)
+    {
+        outputFile << algorithm_names[i] << "\t";
+        // 遍历每一个n值
+        for (int j = 0; j < sizeof(n_values) / sizeof(int); j++)
+        {
+            double total_time = 0.0;
+            int num_iterations = 10000000; // 调整迭代次数
+            int times = sizeof(x_values) / sizeof(double);
+            cout << "正在计算第" << i + 1 << "行, 第" << j + 1 << "列。请耐心等待……" << endl;
+            // 遍历选取的每一个x值
+            for (int t = 0; t < times; t++)
+                // 迭代10^7次
+                for (int k = 0; k < num_iterations; k++)
+                {
+                    clock_t start_time = clock();
+
+                    // 调用当前算法，通过函数接受成员函数指针的方法
+                    algorithms(Polynomials[j], x_values[t], i); // 循环使用x_values
+
+                    clock_t end_time = clock();
+                    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // 以秒为单位
+                    total_time += elapsed_time;
+                }
+            // 计算对于不同x的平均时间
+            double average_time = total_time / times;
+            outputFile << average_time << "\t";
+        }
+
+        outputFile << "\n";
+    }
+
+    // 关闭文件
+    outputFile.close();
+
+    cout << "数据已写入文件" << endl;
+    return 0;
+}
